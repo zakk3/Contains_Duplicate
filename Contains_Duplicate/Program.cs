51905d4195758c9508c6eee8f47ee89a5b0a198e
zakk3 <zaltabouni@gmail.com>
2025-11-09 23:54:14 +0500

    Add files via upload

---
+++ Contains_Duplicate/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Contains_Duplicate;
+
+class Program
+{
+    public bool ContainsDuplicate(int[] nums) 
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++) {
+                    if (nums[i] == nums[j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+}
